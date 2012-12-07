3a4b55eaff86842a22278bc098d837fa729ad1aa
tumbleweedhouses <admin@tumbleweedhouses.com>
2012-12-07 15:35:04 +0200

    Update theresumator-widget.js

--- theresumator-widget.js
+++ theresumator-widget.js
@@ -57,7 +57,3 @@ var script3 = document.createElement('script');script3.type = 'text/javascript';
 
 dropdowncontent.init("houses", "right-bottom", 500, 'mouseover');
 dropdowncontent.init("workshops", "right-bottom", 500, 'mouseover');
-
-if (navigator.appVersion.indexOf('Win')!=-1) {
-  $(#subcontent).css('left','109px !important');
-	} 
